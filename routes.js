f5e6a5a33503c4b28570dafb377829e487b46019
Đô Nguyễn <dont@ghn.vn>
2020-12-17 08:00:50 +0700

    update

--- routes.js
+++ routes.js
@@ -107,7 +107,7 @@ router.post("/get-priority", middlewareAuth, async (req, res) => {
     }
 
     try {
-        priority = deadline_pickup * deadline_pickup_point + distance * distance_point + weight * weight_point
+        priority = deadline_pickup_point * 3 + distance_point * 2 + weight_point
     } catch (error) {
         return res.json({
             success: false,
